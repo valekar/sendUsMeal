f2037d24ce2c4d9d4138015de4820c42448e8166
valekar <srinivas.valekar@gmail.com>
2015-05-16 09:09:42 +0530

    added a few things

---
+++ client/Customer/customer-views/cust-global/cust-global-properties.js
@@ -0,0 +1,16 @@
+Properties ={
+
+    //development
+    NorthIndianCategoryId:"wTER6DGjJS7oCGdaB",
+    SouthIndianCategoryId:"akKvZdKw3S7EFjcrZ",
+    HandPickedCategoryId:"uoZk4KypSTyQCfodW"
+
+    //production
+
+    /* NorthIndianCategoryId:"ec96gzdmLLF6YKAYb",
+     SouthIndianCategoryId:"d9Fwx4FhndH8XTsru",
+     HandPickedCategoryId:"osuBHGuvoba8hhTom"*/
+
+
+
+}
